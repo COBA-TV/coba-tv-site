b91021e31e0328ad3c5085434d41c6e0dea20647
COBA-TV <officialcobatv@gmail.com>
2025-07-24 04:47:42 +0100

    Delete old files

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function toggleSection(id) {
-  var el = document.getElementById(id);
-  if (el.style.display === "none") {
-    el.style.display = "block";
-  } else {
-    el.style.display = "none";
-  }
-}
